efd128d9e3524a7df4150e173ddb76216fc61c6b
roehh <git@roeh.ch>
2023-05-06 16:11:18 +0200

    f

--- download-img.ts
+++ download-img.ts
@@ -1,6 +1,6 @@
 
 const folderPath = './mock/';
-
+let i = 0;
 for await (const file of Deno.readDir(folderPath)) {
   if (file.isFile) {
     const filePath = `${folderPath}/${file.name}`;
@@ -8,12 +8,18 @@ for await (const file of Deno.readDir(folderPath)) {
     if(fileContent.product?.images.nodes)
       for (const {url} of fileContent.product?.images.nodes) {
         console.log(url);
-        fetch(url).then(async (res) => {
-          await Deno.writeFile(`./static/mock/${url.slice(58, -13)}`,  new Uint8Array(await res.arrayBuffer()), {create: true})
+        await fetch(url).then(async (res) => {
+          await Deno.writeFile(`./static/mock/${i}.webp`,  new Uint8Array(await res.arrayBuffer()), {create: true})
         })
+        console.log(`./static/mock/${i}`);
+        
         Deno.readTextFile(filePath).then(res => {
-          Deno.writeTextFile(filePath, res.replaceAll(url, `https://duftsymphonie.ch/mock/${url.slice(58, -13)}`))
+          Deno.writeTextFile(filePath, res.replaceAll(url, `https://duftsymphonie.ch/mock/${i}.webp`))
         })
+        await Deno.readTextFile("./mock/products.json").then(res => {
+          Deno.writeTextFile("./mock/products.json", res.replaceAll(url, `https://duftsymphonie.ch/mock/${i}.webp`))
+        })
+        i++;
       }
   }
 }
